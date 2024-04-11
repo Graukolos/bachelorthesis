5b417800ad40f4c63526bf99f953f7f1fb3865d6
Max Erdelmeier <max.erdelmeier@graukolos.tech>
2024-04-11 12:57:17 +0200

    Update 11.4.

--- code/os-isolated-impl/run.sh
+++ code/os-isolated-impl/run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-
 cargo build --release
-# requires the cpuset package to be installed
-sudo cset shield --cpu 3 --kthread on
-sudo cset shield --shield --verbose
-sudo cset shield --exec "cargo run --release" --user graukolos --group graukolos
-sudo cset shield --reset
+sudo cset shield --cpu 3 --kthread on > /dev/null
+sudo cset shield --exec ./target/release/os-isolated-impl --user graukolos --group graukolos
+sudo cset shield --reset > /dev/null
